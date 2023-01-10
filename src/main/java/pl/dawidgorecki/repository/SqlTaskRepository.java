661b9d698b7b6c2ff711a302e9690f966d915ae3
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2023-01-10 18:53:52 +0100

    Section 5 - SQL, JDBC

--- src/main/java/pl/dawidgorecki/repository/SqlTaskRepository.java
+++ src/main/java/pl/dawidgorecki/repository/SqlTaskRepository.java
@@ -1,9 +1,14 @@
 package pl.dawidgorecki.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.dawidgorecki.model.Task;
 
 @Repository
 public interface SqlTaskRepository extends TaskRepository, JpaRepository<Task, Integer> {
+    @Override
+    @Query(nativeQuery = true, value = "SELECT COUNT(*) > 0 FROM tasks WHERE id=:taskId")
+    boolean existsById(@Param("taskId") Integer id);
 }
