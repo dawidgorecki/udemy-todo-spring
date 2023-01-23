94065c70187399c61e460815872571659c1b88a9
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2023-01-23 12:15:54 +0100

    Section 10 - E2E test

---
+++ src/test/java/pl/dawidgorecki/controller/TaskControllerE2ETest.java
@@ -0,0 +1,44 @@
+package pl.dawidgorecki.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+import pl.dawidgorecki.model.Task;
+import pl.dawidgorecki.repository.TaskRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("integration")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TaskControllerE2ETest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private TaskRepository repository;
+
+    @Test
+    void httpGet_returnsAllTasks() {
+        // given
+        repository.save(new Task("foo", LocalDateTime.now()));
+        repository.save(new Task("bar", LocalDateTime.now()));
+
+        // when
+        Task[] result = restTemplate.getForObject("http://localhost:" + port + "/tasks", Task[].class);
+
+        // then
+        assertThat(result).hasSize(2);
+    }
+
+}
